cc23e713c6d38fe2ad410512f3fa4486ddfcc0ca
ahugla <ahugla@vmware.com>
2019-10-04 10:03:49 +0200

    with sidecontainer

---
+++ sidecontainer_with_agent/container_start.sh
@@ -0,0 +1,23 @@
+# echo input parameters
+echo "LI_VERSION="$LI_VERSION
+echo "LI_SERVER="$LI_SERVER
+
+
+# installe l'agent log insight
+git clone https://github.com/ahugla/LogInsight.git  /tmp/li
+chmod +x /tmp/li/$LI_VERSION/VMware-Log-Insight-*.bin
+/tmp/li/$LI_VERSION/VMware-Log-Insight-*.bin
+rm -rf /tmp/li
+
+
+# configure agent with Log Insight Server FQDN or IP
+sed -i '8 a hostname='$LI_SERVER'' /var/lib/loginsight-agent/liagent.ini
+
+
+# start Log Insight agent
+/etc/init.d/liagentd start
+
+
+# pour que le process ne s'arrete pas et que le container ne termine pas
+tail -f /dev/null
+
